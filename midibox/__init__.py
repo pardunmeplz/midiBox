0a785a69d1535b9a381e567f46bd6a5ec47db72e
pardunmeplz <dundh.parth@gmail.com>
2022-12-01 00:22:21 +0530

    Changed libraries from pyaudio to librosa

--- midibox/__init__.py
+++ midibox/__init__.py
@@ -1,16 +1,6 @@
-import wave
+import librosa
 import matplotlib.pyplot as plt
-import numpy as np
 
-
-with wave.open("audio/pfft.wav",'rb') as audio:
-    samplQ = audio.getframerate()
-    nSamples = audio.getnframes()
-    signal = audio.readframes(-1)
-
-time = nSamples/samplQ
-signalArray = np.frombuffer(signal,dtype=np.int16)
-times = np.linspace(0,time,num=nSamples*2)
-
-plt.plot(times,signalArray)
+y, sr = librosa.load('audio/pfft.wav')
+plt.plot(y)
 plt.show()
